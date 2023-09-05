f3bc8139e9aac85c484c67eb39a2f5a057522ef8
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-09-05 14:50:06 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Aleyna Aktaş on 5.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
